5d356c172f1269ea1b55d9f0ceccbd2604e41d6a
matheus <matheusmsantos15@gmail.com>
2019-06-11 17:04:02 -0300

    js primeiro dia

---
+++ hh
@@ -0,0 +1,17 @@
+= atribuiçao 
+== igual
+=== compara o valor e o atributio
+let== criar uma variavel
+CONCATENAÇAO = SOMAR TEXTO
+LET NOME + LET SOBRENOME == CONCATENAR
+[""] == PARA TEXTO
+PODE CRIAR CONSTANTE DENTRO DE UMA LISTA== NAUM
+variavel=var
+let faz tudo q o var faz, tudo mesmo,NAUM
+contador e igual a I
+laço de repitaçao = for tem mais coisas que o if 
+se o seu computador travar da um ctrl + alt + delete
+length criador de variaveix
+for in percorre os indices
+for off percorre os valores
+
---
+++ script.js
@@ -0,0 +1,59 @@
+//entrada de dados
+//let nome = prompt("digite aqui seu nome e seja feliz")
+
+
+//saida de dados
+//alert(nome);
+//console.log(nome);
+//entrada e saida de dados
+//confirm(e ai, aceita ou não)//
+
+
+
+//let media = prompt("digite aqui a sua media ");
+
+//if(media>10 || media<0){
+    //alert("nota invalida")
+//}
+
+//else if(media>=7){
+    //alert('aprovado');
+
+//}
+
+//else if(media<7 && media>=5){
+   // alert("recuperaçao");
+//}
+
+
+
+//else{
+   // alert('reprovado');//
+//}
+
+let nome = "matheus";
+let contador = 0;
+
+//while(contador<4){;
+    //qconsole.log(nome);
+    //contador++;
+//}
+
+
+//for(let i=0; i<4; i++){
+  //  console.log("matheus");
+//}
+
+//for(let j=10; j>0; j --){
+  //  console.log("iiiiihhhhuuuuuuuuuu");
+//}
+
+
+
+let signos = ["ares "," carsêr","acuario","peiches",'sargitouro',"sargitario"];
+
+for(let valores in signos){
+    console.log("o valor da posiçao " + valores + " e o signo dessa posiçao é " + signos[valores]);
+}
+
+
